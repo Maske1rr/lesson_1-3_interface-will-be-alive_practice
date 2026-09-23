b567aab3330a35976ff053dbcb0179ce6d3bae9f
Maske1rr <67798652+Maske1rr@users.noreply.github.com>
2026-09-23 03:32:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,28 @@
 "use strict";
 
-const firstCard = document.querySelector('[data-event="talk"]');
 const labStatus = document.querySelector("#lab-status");
 
 const detailsTitle = document.querySelector("#details-title");
+const detailsDescription = document.querySelector("#details-description");
+const detailsTime = document.querySelector("#details-time");
 
-const eventName = "Город в деталях";
-let clickCount = 0;
-
-firstCard.addEventListener("click", () => {
-  clickCount = clickCount + 1;
-
-  if (clickCount === 1) {
-    labStatus.textContent = `Первое открытие: ${eventName}`;
-  } else {
-    labStatus.textContent = `Повторное открытие: ${eventName}`;
-  }
-
-  detailsTitle.textContent = eventName;
-});
-
-const secondCard = document.querySelector('[data-event="workshop"]');
-
-secondCard.addEventListener("click", () => {
-  detailsTitle.textContent = "Мастерская постеров";
-});
 const cards = document.querySelectorAll(".event-card");
 const eventCount = document.querySelector("#event-count");
 
 eventCount.textContent = cards.length;
 
+function showEvent(card) {
+  detailsTitle.textContent = card.dataset.title;
+  detailsDescription.textContent = card.dataset.description;
+  detailsTime.textContent = card.dataset.time;
+}
+
 cards.forEach((card) => {
   card.addEventListener("click", () => {
     labStatus.textContent = "Нажата карточка из программы";
+    showEvent(card);
   });
 });
-
-// 08. Напишете функцию showEvent(card) и возьмете данные из HTML.
 
 // 09. Добавите выбранное состояние, акцент и CSS-анимацию.
 
